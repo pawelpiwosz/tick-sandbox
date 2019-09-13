1c4bda99a1768962e340bae8dc260a8e8a24af23
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-09-13 19:27:06 +0000

    Fix stack problems

--- scripts/cleanall.sh
+++ scripts/cleanall.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 docker-compose down
-docker rmi influxdb:ver telegraf:ver kapacitor:ver chronograf:ver
+docker rmi influxdb1:ver influxdb2:ver telegraf:ver kapacitor:ver chronograf:ver relay1:ver relay2:ver
 docker rmi $(docker images -f "dangling=true" -q)
 rm -rf kapacitor/data influxdb/data chronograf/data
 echo -e '\n\n\nAll data cleaned up!\n\n\n'
